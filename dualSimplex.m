4eea14c8e8f2edf982bd1414a5f6c2dbc71cd344
Akshat Sarin <akshatsarin14@gmail.com>
2026-05-06 13:18:55 +0530

    Update dualSimplex.m

--- dualSimplex.m
+++ dualSimplex.m
@@ -1,65 +1,56 @@
-clc
-clear all
+clc;
+clear all;
 
-c = [-2 0 -1];
-a = [-1 -1 1;-1 2 -4];
-b = [-5; -8];
-
-n = size(a, 2);
-s = eye(size(a, 1));
-
-A = [a s b];
-
+%% Initial Table Formation
+a = [-1, -1, 1; -1, 2, -4]; % Constraints Coefficient
+B = [-5; -8]; % Constraints Solution
+C = [-2, 0, -1]; % Objective Function Coefficient
+n = 3; % No. of variables excluding slack variable
+s = eye(size(a, 1)); % Slack Variable Coefficient
+A = [a, s, B];
 cost = zeros(1, size(A, 2));
-cost(1:n) = c;
+cost(1 : n) = C;
+BV = n + 1 : 1 : size(A, 2) - 1;
+zjcj = cost(BV) * A - cost;
+Z = [A; zjcj];
+Z = array2table(Z, 'VariableNames', {'x1', 'x2', 'x3', 's1', 's2', 'sol'});
+Z
 
-bv = n+1 : size(A, 2)-1;
-
-zjcj = cost(bv) * A - cost;
-zcj = [A; zjcj];
-
-simptable = array2table(zcj);
-simptable.Properties.VariableNames(1:size(zcj,2)) = ...
-    {'x1','x2','x3','s1','s2','Sol'}
-run=true
-while run
-    zc=zjcj(1:end-1);
-    if all(zc>=0)
-        disp('optimality achieved...Go to dual simplex')
+%% Dual Simplex Iteration
+Run = true;
+while Run
+    zc = zjcj(1 : end - 1);
+    if all(zc >= 0)
+        fprintf('The current solution is optimal');
     else
-        disp('not optimal')
+        fprintf('Dual Simplex can not be applied');
         break;
     end
-    sol=A(:,end);
-    if any(sol<0)
-        [minval pvt_row]=min(sol)
-        for i=1:size(A,2)-1
-            if A(pvt_row,i)<0
-                ratio(i)=abs(zjcj(i)/A(pvt_row,i))
-            else
-                ratio(i)=inf
+    sol = A(:, end);
+    if any(sol < 0)
+        [Minimal, pvt_row] = min(sol);
+        for i = 1 : size(A, 2) - 1
+            if A(pvt_row, i) < 0
+                ratio(i) = abs(zjcj(i) / A(pvt_row, i));
+            else 
+                ratio(i) = Inf;
             end
         end
-        [minval pvt_col]=min(ratio)         
+        [minratio, pvt_col] = min(ratio);
         pvt_key = A(pvt_row, pvt_col);
-        bv(pvt_row) = pvt_col;
-
-        A(pvt_row, :) = A(pvt_row, :) / pvt_key;
-
-        for i = 1:size(A,1)
+        BV(pvt_row) = pvt_col;
+        A(pvt_row, :) = A(pvt_row, : ) / pvt_key;
+        for i = 1 : size(A, 1)
             if i ~= pvt_row
                 A(i, :) = A(i, :) - A(i, pvt_col) * A(pvt_row, :);
             end
         end
-
         zjcj = zjcj - zjcj(pvt_col) * A(pvt_row, :);
-        zcj = [A; zjcj]
-
-        simptable = array2table(zcj);
-        simptable.Properties.VariableNames(1:size(zcj,2)) = {'x1','x2','x3','s1','s2','Sol'}
-
+        Z = [A; zjcj];
+        Z = array2table(Z, 'VariableNames', {'x1', 'x2', 'x3', 's1', 's2', 'sol'});
+        Z
     else
-        run = false;
-        fprintf('optimal sol')
+        Run = false;
+        fprintf('\nThe current solution is feasible');
     end
 end
